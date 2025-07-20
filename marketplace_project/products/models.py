db479593f966e084f3c864654daa72bc6b7cd299
Artem <sambist161008@gmail.com>
2025-07-21 00:30:13 +0300

    set3

--- marketplace_project/products/models.py
+++ marketplace_project/products/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    seller = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='products'
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='products'
+    )
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
--- marketplace_project/products/views.py
+++ marketplace_project/products/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from .models import Product, Category
+from .forms import ProductForm
+
+
+class ProductListView(ListView):
+    template_name = 'products/product_list.html'
+    model = Product
+    context_object_name = 'products'
+    paginate_by = 12
+
+    def get_queryset(self):
+        queryset = Product.objects.all().select_related('category', 'seller')
+        category_id = self.request.GET.get('category')
+        if category_id:
+            queryset = queryset.filter(category_id=category_id)
+        return queryset
+
+
+class ProductDetailView(DetailView):
+    template_name = 'products/product_detail.html'
+    model = Product
+    context_object_name = 'product'
+
+
+class ProductCreateView(CreateView):
+    template_name = 'products/product_form.html'
+    form_class = ProductForm
+    success_url = '/products/'  # або reverse_lazy
+
+    def form_valid(self, form):
+        form.instance.seller = self.request.user
+        return super().form_valid(form)
+
+
+class ProductUpdateView(UpdateView):
+    template_name = 'products/product_form.html'
+    form_class = ProductForm
+    model = Product
+    context_object_name = 'product'
+    success_url = '/products/'
+
+    def get_queryset(self):
+        return Product.objects.filter(seller=self.request.user)
+

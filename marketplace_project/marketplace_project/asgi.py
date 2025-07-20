dd1869a624f7b5545304669f239cef2916e1f266
Artem <sambist161008@gmail.com>
2025-07-21 00:15:20 +0300

    set1

--- marketplace_project/marketplace_project/asgi.py
+++ marketplace_project/marketplace_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for market project.
+ASGI config for marketplace_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'market.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'marketplace_project.settings')
 
 application = get_asgi_application()
--- marketplace_project/marketplace_project/urls.py
+++ marketplace_project/marketplace_project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for market project.
+URL configuration for marketplace_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
+    path('products/', include('products.urls')),
+    path('orders/', include('orders.urls')),
+    path('reviews/', include('reviews.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
